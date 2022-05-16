afb093018ead16a95a0a9a6cdbb2b3414ec18252
marcelo-gomj <marcelomgj005@gmail.com>
2022-05-16 11:34:03 -0300

    update: first component test

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,26 +1,47 @@
-import { getByText, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { ActiveLink } from '.';
 
+// simulate the router module
 jest.mock('next/router', () => {
-    return {
-        useRouter () {
-            return {
-                asPath: '/'
-            }
-        }
-    }
+   return {
+      useRouter() {
+         return {
+            asPath: '/'
+         }
+      }
+   }
 })
 
-test('active link renders correctly', () => {
-    const { debug, getByText } = render(
-        
-        <ActiveLink 
-            activeClassName="active" 
-            href="/"        
-        >
+//add category for test
+describe('ActiveLink component', () => {
+   it('renders correctly', () => {
+      const { debug, getByText } = render(
+
+         <ActiveLink
+            activeClassName="active"
+            href="/"
+         >
             <a>Home</a>
-        </ActiveLink>
-    )
+         </ActiveLink>
+      )
+      
+      // Analize the component virtual
+      expect(getByText('Home')).toBeInTheDocument();
+   })
 
-   expect(getByText('Home')).toBeInTheDocument();
+   it('adds active class if the link as currently active', () => {
+      const { getByText } = render(
+
+         <ActiveLink
+            activeClassName="active"
+            href="/"
+         >
+            <a>Home</a>
+         </ActiveLink>
+      )
+      
+      // Analize the component virtual
+      expect(getByText('Home')).toHaveClass('active');
+   })
 })
+
