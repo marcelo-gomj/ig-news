07546071873b93d4dbdf3a699d2756bff984794e
marcelo-gomj <marcelomgj005@gmail.com>
2022-06-28 18:30:50 -0300

    add: post test

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react';
 import Post, { getServerSideProps } from '../../pages/posts/[slug]';
 import { mocked } from 'ts-jest/cli';
 import { getPrismicClient } from '../../Services/prismic'
+import { getSession } from 'next-auth/react';
+import JSDOMEnvironment from 'jest-environment-jsdom';
 
 const post = 
     {
@@ -26,23 +28,61 @@ describe('Posts page', () => {
     });
 
     it('redirects user if no subscription is found', async () => {
+        const getSessionMocked = mocked(getSession);
+        getSessionMocked.mockResolvedValueOnce(null); 
 
         const response = await getServerSideProps({
-            req : {
-                cookies : {},
-            },
-        }as any);
+            params : { slug : 'my-new-post' }
+        } as any);
 
         // Checa se o objeto da resposta tem o mesmo valor
         // Checagem não rigorosa com o .objectContaining
         expect(response).toEquals(
             expect.objectContaining({
-                redirect: {
+                redirect: expect.objectContaining({
                     destination: '/'
-                }
+                })
             })
         )
     })
 
+    it('loads initial data', async () => {
+        const getSessionMocked = mocked(getSession);
+        const getPrismicClientMocked = mocked(getPrismicClient);
+
+        getPrismicClientMocked.mockReturnValueOnce({
+            getByUID: jest.fn().mockResolvedValue({
+                data : {
+                    title: [{ type: 'heading', text : 'My new post'}],
+                    content: [{ type: 'paragraph', text : 'Post excerpt'}],
+                },
+                last_publication_date : '04-01-2022' 
+            })
+        } as any)
+
+        getSessionMocked.mockResolvedValueOnce({
+            activeSubscription: 'fake-active-subscription'
+        } as any );
+
+        const response = await getServerSideProps({
+            params : { slug : 'my-new-post' }
+        } as any);
+
+        // Checa se o objeto da resposta tem o mesmo valor
+        // Checagem não rigorosa com o .objectContaining
+        expect(response).toEquals(
+            expect.objectContaining({
+                props : {
+                    post: {
+                        slug :  'my-new-post', 
+                        title : 'My new post',
+                        content : '<p>Post content</p>',
+                        updatedAt: '01 de abril de 2022'
+                    }
+                }
+            })
+        )
+      
+    })
 
 })
